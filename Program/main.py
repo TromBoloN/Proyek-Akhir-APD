da4938f762205018a11fb3010ac0bf5c418b52dd
TromBoloN <arifvt1212@gmail.com>
2025-11-21 13:46:11 +0800

    Menambah Function Mod dan Perbaikan menu.py - Arif

--- Program/main.py
+++ Program/main.py
@@ -1,5 +1,35 @@
 import os
-os.system()
+from auth import login
+os.system('cls')
 
 while True:
-    clear()
+    os.system('cls')
+    print("=== Menu Utama ===")
+    print("1. Login")
+    print("2. Register")
+    print("3. Keluar")
+    pilihanMenu = input("Pilih menu (1/2/3): ")
+    
+    #if pilihanMenu == '1':
+        #login()
+    # while True:
+    #     os.system('cls')
+    #     if roleLogin == 'superadmin':
+    #         # Menu Superadmin 
+    #     elif roleLogin == 'admin':
+    #         # Menu Admin
+    #     elif roleLogin == 'user':
+    #         # Menu User
+    #     else:
+    #         print("Role tidak dikenali.")
+    #         break
+    #elif pilihanMenu == '2':
+        #register()
+    
+    if pilihanMenu == '3':
+        print("Terima kasih telah menggunakan program ini.")
+        break
+    else:
+        print("Pilihan tidak valid. Silakan coba lagi.")
+        input("Tekan Enter untuk melanjutkan...")
+        continue
